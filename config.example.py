77fc678ed4786ecff7e7297927a667252b12a397
Jrabbit <jackjrabbit+github@gmail.com>
2011-01-01 00:32:57 -0600

    re-factored code so that my oauth keys are in config.py
    including twitter.py & get_access_token.py of python-twitter under Apache2 License.

---
+++ config.example.py
@@ -0,0 +1,6 @@
+#Tasktwit oauth creds
+consumer_key = 'eFbUI4ee3Zb1xpWMoi95A'
+consumer_secret = '7PBZdw0QBZb7JtRGdUQRvMpTm9WKUJ9pWSZdAwv1cDE'
+#User editables below.
+authorized_users = ['jackolas']
+#user creds go here. run get_access_token.py to get the access_token_key & access_token_secret
---
+++ tasktwit.py
@@ -0,0 +1,19 @@
+import twitter
+import sys
+import TheHitList
+from conf import *
+
+api = twitter.Api(consumer_key,consumer_secret, access_token_key,
+access_token_secret)
+
+if api.GetDirectMessages():
+    for msg in api.GetDirectMessages():
+        if str(msg.sender_screen_name) in authorized_users:
+            add_task(msg.AsDict())
+
+def add_task(msg):
+    task = msg.text
+    thl = TheHitList.Application()
+    thl_task = TheHitList.Task()
+    thl_task.title = task
+    thl.inbox().add_task(thl_task)
