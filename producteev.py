2a528c5a6240b7791c6f1e0bbb40385dc12e389b
Jrabbit <jackjrabbit+github@gmail.com>
2011-01-01 02:16:37 -0600

    productive REST api helper file... auth works.

---
+++ producteev.py
@@ -0,0 +1,45 @@
+#import oauth2 as oauth
+import urllib2
+import urllib
+import hashlib
+import getpass
+import json
+
+API_KEY ='4610d9ec2da135a2af02663519e7d2a3'
+API_SECRET = '5ae73f6172fbaa3aa6244897a4a0cdae'
+user = 'jackjrabbit@gmail.com' #edit this or supply it.
+url = 'https://api.producteev.com/'
+
+
+def auth():
+    password = getpass.getpass('Your producteev password:')
+    raw_values = {'email': user, 'password': password, 'api_key': API_KEY}
+    data = hancock(raw_values)
+    # print values
+    login_json = urllib2.urlopen(url + 'users/login.json', data).read()
+    print login_json
+    print "Authenticated!"
+    return json.loads(login_json)['login']['token']
+
+def hancock(values):
+    """take a dict of REST data and make md5 signature"""
+    phrases = []
+    for x,y in values.iteritems():
+        phrases.append(x+y)
+    phrases.sort() 
+    hash_me =  ''.join(phrases) + API_SECRET
+    #cat the phrases together
+    signature = hashlib.md5(hash_me).hexdigest()
+    values['api_sig'] = signature
+    data = urllib.urlencode(values)
+    return data
+
+def get_tasks(token):
+    raw_values = {'token': token, 'api_key': API_KEY}
+    data = hancock(raw_values)
+    tasks_json = urllib2.urlopen(url + 'tasks/show_list.json', data).read()
+    print json.loads(tasks_json)
+
+
+if __name__ == '__main__':
+    auth()
